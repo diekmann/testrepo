50ffbf489bfdd153725056f684a67d5325a7ddf5
Cornelius Diekmann <1486029+diekmann@users.noreply.github.com>
2024-09-17 15:15:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "google_project" "my_project" {
   name       = "My Project"
   project_id = "my-project-id849y3iu221y"
   org_id     = "969273005403"
+  billing_account     = "0167A6-E642E3-A16FE7"
 }
 
 resource "google_sql_database_instance" "main" {
