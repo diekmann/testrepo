45b3817de5009d5c27e54d7c5bec4967e20c78ad
Cornelius Diekmann <1486029+diekmann@users.noreply.github.com>
2024-09-17 12:11:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# https://docs.spacelift.io/integrations/cloud-providers/gcp
+provider "google" {}
+
 resource "google_project" "my_project" {
   name       = "My Project"
   project_id = "my-project-id849y3iu221y"
