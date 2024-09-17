500967f0fada7a00c01fd0b1dc454d6b0c34e422
Cornelius Diekmann <1486029+diekmann@users.noreply.github.com>
2024-09-17 14:16:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "google_project" "my_project" {
   name       = "My Project"
   project_id = "my-project-id849y3iu221y"
   org_id     = "969273005403"
+  billing_account = data.google_billing_account.acct.id
 }
 
 resource "google_sql_database_instance" "main" {
@@ -19,4 +20,12 @@ resource "google_sql_database_instance" "main" {
     # type. See argument reference below.
     tier = "db-f1-micro"
   }
+}
+
+data "google_billing_account" "acct" {
+  display_name = "Core -> 3 Days Any Org Limited Use Billing"
+}
+
+output "billingleaker" {
+  value = data.google_billing_account.acct.id
 }
